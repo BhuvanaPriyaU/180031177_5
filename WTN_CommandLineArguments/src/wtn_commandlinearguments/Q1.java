51e8a9fafaba476e01aaa7015eaaf274f3e444cd
UPPALA <UPPALA@DESKTOP-R7SMQG5>
2021-05-19 22:21:50 +0530

    Wipro(WTN) - Command Line Arguments - Practice

---
+++ WTN_CommandLineArguments/src/wtn_commandlinearguments/Q1.java
@@ -0,0 +1,7 @@
+package wtn_commandlinearguments;
+
+public class Q1 {
+	public static void main(String args[]) {
+		System.out.println(args[0]+" Technologies "+args[1]);
+	}
+}
---
+++ WTN_CommandLineArguments/src/wtn_commandlinearguments/Q3.java
@@ -0,0 +1,10 @@
+package wtn_commandlinearguments;
+
+public class Q3 {
+	public static void main(String args[]) {
+		int n1=Integer.parseInt(args[0]);
+		int n2=Integer.parseInt(args[1]);
+		int sum=n1+n2;
+		System.out.printf("The sum of %d and %d is %d",n1,n1,sum);
+	}
+}
